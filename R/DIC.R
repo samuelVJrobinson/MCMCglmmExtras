84426ee50561489fb2cadfef362010fb25b96739
Sam Robinson <samuel.vj.robinson@gmail.com>
2017-04-12 21:54:44 -0600

    Added help info for document()

--- R/DIC.R
+++ R/DIC.R
@@ -1,4 +1,14 @@
-DIC=function(m1,...){ #Convenience function to extract DIC values from MCMCglmm models
+#' Deviance Information Criteria (DIC)
+#'
+#' Extracts DIC from a individual or set of MCMCglmm models. If a set of models is provided, provides a table of DIC differences between candidate models.
+#' @param m1 MCMCglmm model
+#' @param ... Other MCMCglmm models
+#' @export
+#' @examples
+#' DIC(mod1)
+#' DIC(mod1,mod2)
+
+DIC=function(m1,...){
   modlist=list(m1,...)
   dicvals=unlist(lapply(modlist,'[[',c('DIC')))
   fixeffs=as.character(lapply((lapply(modlist,'[[','Fixed')),'[[','formula'))
